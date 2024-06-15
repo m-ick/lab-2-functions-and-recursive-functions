1e91128da78e875e6baa8e4c05c678a8a9a53ff9
m-ick <11mickm@gmail.com>
2024-06-15 11:58:52 +0000

    1: Simple loops

--- main.cpp
+++ main.cpp
@@ -31,17 +31,29 @@ int t_n = 0;
 
 int max_n = 10;
 
+int fib(int n){   
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    else
+        return(fib(n-1) + fib(n-2));
+}
+
 int main()
 {
     // 1: Simple loops
     printf("Fibonacci sequence for %d numbers\r\n", max_n);
+    printf("Number %d: %d \r\n", 1, 0); 
+    printf("Number %d: %d \r\n", 2, 1);
 
-    for( int i=0; i<max_n; i++){
+    for( int i=2; i<max_n; i++){
         blink = 0; // turn led off
-        printf("Number %d: %d \r\n", i+1, t_n);
+        
         t_n = t_1 + t_2;
         t_1 = t_2;
         t_2 = t_n;
+        printf("Number %d: %d \r\n", i+1, t_n);
 
         thread_sleep_for(blink_period);
         blink = 1; // turn led on
@@ -49,17 +61,7 @@ int main()
         
     }
 
-    // // 2: Recursive loop
-
-    // int fib(int n){   
-    // if(n == 0)
-    //     return 0;
-    // if(n == 1)
-    //     return 1;
-    // else
-    //     return(fib(n-1) + fib(n-2));
-    // }
-
+    // 2: Recursive loop
     // printf("Finbonachi sequence for %d numbers using a recursive function\r\n", max_n);
     // for(int i=0; i<max_n; i++){
     //     printf("Number %d: %d \r\n", i+1, fib(i));
